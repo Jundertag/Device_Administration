fa921d0fb2c92e6456615fdf4c663260b5306592
Jundertag <jaydenmacarthur3@gmail.com>
2026-05-15 17:37:16 -0300

    use TypeConverter to convert between List<String> and String

--- app/src/main/java/com/jayden/deviceadministration/data/room/network/NetworkLogDatabase.kt
+++ app/src/main/java/com/jayden/deviceadministration/data/room/network/NetworkLogDatabase.kt
@@ -2,12 +2,23 @@ package com.jayden.deviceadministration.data.room.network
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
 @Database(entities = [
     TcpNetworkEventEntity::class,
     DnsNetworkEventEntity::class
 ], version = 1)
+@TypeConverters(Converters::class)
 abstract class NetworkLogDatabase : RoomDatabase() {
     abstract fun tcpNetworkEventDao(): TcpNetworkEventDao
     abstract fun dnsNetworkEventDao(): DnsNetworkEventDao
+}
+
+class Converters {
+    @TypeConverter
+    fun fromStringList(list: List<String>): String = list.joinToString("|||")
+
+    @TypeConverter
+    fun toStringList(value: String): List<String> = value.split("|||")
 }
